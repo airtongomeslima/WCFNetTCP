0c0ca7bc5158fb1a3a19eba9942ed95dfeccafdd
airtongomeslima <airtongomesdelima@hotmail.com>
2018-07-30 07:27:19 -0300

    Final

---
+++ WCFClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFContracts;
+
+namespace WCFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("abrir conexão");
+            Console.ReadLine();
+            var uri = "http://localhost:9015/ProductService";
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+
+            for (int i = 0; i < 20000; i++)
+            {
+                    using (var channel = new ChannelFactory<IProductService>(binding))
+                    {
+                        var endPoint = new EndpointAddress(uri);
+                        var proxy = channel.CreateChannel(endPoint);
+                        for (int a = 0; a < 3; a++)
+                        {
+                            proxy?.GetStrings().ToList().ForEach(p => Console.WriteLine(i + " - " + p + "- - " + a));
+                        }
+                        channel.Close();
+                        proxy = null;
+                    }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- WCFTest/Program.cs
+++ WCFTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WCFContracts;
 
 namespace WCFTest
 {
@@ -10,6 +12,24 @@ namespace WCFTest
     {
         static void Main(string[] args)
         {
+            var uris = new Uri[1];
+            string addr = "http://localhost:9015/ProductService";
+            uris[0] = new Uri(addr);
+
+
+            ServiceHost host = new ServiceHost(typeof(ProductService), uris);
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+            host.AddServiceEndpoint(typeof(IProductService), binding, "");
+            host.Opened += HostOpened;
+            host.Open();
+
+            Console.ReadLine();
+            host.Close();
+        }
+
+        private static void HostOpened(object sender, EventArgs e)
+        {
+            Console.WriteLine("WCF service is started");
         }
     }
 }
